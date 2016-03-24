3b2d13fb6880e240e0d11b50dafcf11b81085f0a
Nik Stankovic <nik2015@niks-lab.com>
2016-03-24 22:36:12 +0800

    add all files

--- optimizeACL.pl
+++ optimizeACL.pl
@@ -2,9 +2,11 @@
 
 #############################################################################
 #                                                                           #
-# countIP.pl                                                                #
+# optimizeACL.pl                                                            #
 #                                                                           #
-# Shell script / filter reads in an ACL returns sum total number of IPs     #
+# Will optimize an ACL                                                      #
+# Works like a filter, taking an ACL on STDIN,                              #
+# and printing the optimized ACL out on STDOUT                              #
 #                                                                           #
 # Nik Stankovic, Version 1.0 on April 28, 2010                              #
 #                                                                           #
@@ -12,12 +14,17 @@
 
 use ACL;
 
-$sum = 0;
-
 while ( <> ) {
-    if( isValidSubnet( $_ ) ) { $sum = $sum + subnetSize( $_ ) }
+    push( @ACL, $_ );    
 }
 
-print "Total IPs: $sum\n";
+#print "Read in $#ACL records...\n";
+
+@optimized_ACL = aclOptimize( @ACL );
+
+foreach $a ( @optimized_ACL ) { print "$a" }
 
 #################################### END ####################################
+
+
+
